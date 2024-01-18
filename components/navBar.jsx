124ca6a23f05172bcb3eeab080360dd540588b2a
Iyyafi Qolbi <iyyafi@gmail.com>
2024-01-18 08:55:25 +0000

    update nav element

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -14,29 +14,28 @@ export default function NavBar() {
   const isActive = (value) => {
     return sort === value;
   };
+
   return (
-    <div
-      id="navbarWrapper"
-      className="flex flex-row items-center justify-between rounded border-b border-[#ccc] px-3 py-2.5"
+    <nav
+      id="navbarBox"
+      className="flex gap-1 border-b border-[#ccc] px-3 py-2.5"
     >
-      <nav id="navbarBox" className="flex gap-1">
-        {sortmenu.map((menu) => (
-          <Link
-            key={menu.value}
-            href={{
-              pathname: "/",
-              query: { sort: menu.value },
-            }}
-            className={`rounded-full  text-sm py-2 px-4 ${
-              isActive(menu.value)
-                ? "bg-[#D2DADD] text-[#0F1A1C]"
-                : "text-[#576F76]"
-            }`}
-          >
-            {menu.label}
-          </Link>
-        ))}
-      </nav>
-    </div>
+      {sortmenu.map((menu) => (
+        <Link
+          key={menu.value}
+          href={{
+            pathname: "/",
+            query: { sort: menu.value },
+          }}
+          className={`rounded-full  text-sm py-2 px-4 ${
+            isActive(menu.value)
+              ? "bg-[#D2DADD] text-[#0F1A1C]"
+              : "text-[#576F76]"
+          }`}
+        >
+          {menu.label}
+        </Link>
+      ))}
+    </nav>
   );
 }
